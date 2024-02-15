7cff96b70382816f38a6c4b77634cf8e54cf8a83
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-02-15 18:56:34 -0300

    Adds darkening of squares upon hovering

--- script.js
+++ script.js
@@ -13,13 +13,26 @@ function loadBoard(gridCount, boardSize) {
     
         newSquare.addEventListener('mouseenter', (event) => {
 
-            const redValue = Math.floor(Math.random() * 256);
-            const greenValue = Math.floor(Math.random() * 256);
-            const blueValue = Math.floor(Math.random() * 256);
+            const squareFilter = event.target.style['filter'];
+            const brightnessValue = squareFilter.split('').filter(
+                (value) => !isNaN(value) || value === "."
+            ).join('');
+            
+            if (!brightnessValue) {
+                const redValue = Math.floor(Math.random() * 256);
+                const greenValue = Math.floor(Math.random() * 256);
+                const blueValue = Math.floor(Math.random() * 256);
 
-            event.target.style['background-color'] = `rgb(
-                ${redValue}, ${greenValue}, ${blueValue}
-            )`;
+                event.target.style['background-color'] = `rgb(
+                    ${redValue}, ${greenValue}, ${blueValue}
+                )`;
+
+                event.target.style['filter'] = 'brightness(1)';
+            } else {
+                event.target.style['filter'] = `brightness(
+                    ${Math.max(Number(brightnessValue)-0.1, 0)}
+                )`;
+            }
         });
     
         container.appendChild(newSquare);
